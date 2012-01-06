8e9427c118244c5bad6ba2615aae2bea687f8d1f
Lolmewn <Lolmewn@SybrensPC>
2012-01-06 10:56:19 +0100

    Test

---
+++ src/main/java/nl/lolmewn/Main.java
@@ -0,0 +1,15 @@
+package nl.lolmewn;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin{
+
+	public void onDisable() {
+		
+	}
+
+	public void onEnable() {
+		
+	}
+
+}
